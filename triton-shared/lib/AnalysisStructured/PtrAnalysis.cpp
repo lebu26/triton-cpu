3db995e29df6aba53824f9e1742d169c92b44e78
Daniel <danielantonio.martinezs@um.es>
2025-08-21 17:52:33 +0800

    upstream commit 41fce4c

--- triton-shared/lib/AnalysisStructured/PtrAnalysis.cpp
+++ triton-shared/lib/AnalysisStructured/PtrAnalysis.cpp
@@ -711,7 +711,8 @@ LogicalResult PtrAnalysis::visitOperand(Value operand, PtrState &state,
       } else if (auto intToPtrOp = dyn_cast<triton::IntToPtrOp>(op)) {
         return visitOperandIntToPtr(intToPtrOp, state, loc, builder);
       } else if (auto makeTensorOp = dyn_cast<triton::MakeTensorPtrOp>(op)) {
-        llvm_unreachable("Unexpected operand defining operation tts.make_tptr");
+        op->emitRemark("Unexpected defining op for triton pointer operand");
+	return failure();
       } else {
         llvm_unreachable("Unexpected operand defining operation");
       }
